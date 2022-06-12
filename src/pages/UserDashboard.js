00342ae436ad3d94b993e4d289a870ab4cc8e2d6
Eric Lantz <77892536+ericmlantz@users.noreply.github.com>
2022-06-12 10:03:57 -0400

    Made Dashboard User Page with chat container and matches swiping area

--- src/pages/UserDashboard.js
+++ src/pages/UserDashboard.js
@@ -1,11 +1,37 @@
 import axios from 'axios'
 import { useCookies } from "react-cookie"
 import {useEffect, useState} from 'react'
+import TinderCard from 'react-tinder-card'
+import ChatContainer from '../components/ChatContainer'
 
 const UserDashboard = () => {
   const [user, setUser] = useState(null)
   const [cookies, setCookie, removeCookie] = useCookies(['user'])
+  const [lastDirection, setLastDirection] = useState()
 
+  const characters = [
+    {
+      name: 'Richard Hendricks',
+      url: './img/richard.jpg'
+    },
+    {
+      name: 'Erlich Bachman',
+      url: './img/erlich.jpg'
+    },
+    {
+      name: 'Monica Hall',
+      url: './img/monica.jpg'
+    },
+    {
+      name: 'Jared Dunn',
+      url: './img/jared.jpg'
+    },
+    {
+      name: 'Dinesh Chugtai',
+      url: './img/dinesh.jpg'
+    }
+  ]
+  
   const userId = cookies.UserId
 
   const getUser = async () => {
@@ -23,8 +49,40 @@ const UserDashboard = () => {
   }, [])
 
   console.log('user', user)
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
+
   return (
-    <div>UserDashboard</div>
+    <div className='dashboard'>
+      <ChatContainer/>
+      <div className='swipe-container'>
+        <div className='card-container'>
+          
+          {characters.map((character) =>
+            <TinderCard className='swipe' 
+            key={character.name} 
+            onSwipe={(dir) => swiped(dir, character.name)} 
+            onCardLeftScreen={() => outOfFrame(character.name)}>
+              <div style={{ backgroundImage: 'url(' + character.profile_photo + ')' }} className='card'>
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          )}
+          <div className='swipe-info'>
+            {lastDirection ? <p>You swiped {lastDirection}</p> : <p/>}
+          </div>
+
+
+        </div>
+      </div>
+    </div>
   )
 }
 export default UserDashboard
