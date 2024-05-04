663baa12259e102baa04baf7ed14184e53f1a9e1
ericmlantz <ericmlantz@gmail.com>
2024-05-04 12:47:14 -0400

    Update to tinder-react-cards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 //Library Imports
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import { useCookies } from "react-cookie";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
 
 //Component/Page Imports
 import Home from './pages/Home'
 import UserOnboarding from './pages/UserOnboarding'
 import RestOnboarding from './pages/RestOnboarding'
-import RestDashboard from "./pages/RestDashboard"
-import UserDashboard from "./pages/UserDashboard"
+import RestDashboard from './pages/RestDashboard'
+import UserDashboard from './pages/UserDashboard'
 
 //API Imports
 
 function App() {
-
   const [cookies, setCookie, removeCookie] = useCookies(['user'])
 
   const authToken = cookies.AuthToken
@@ -20,14 +19,22 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path='/' element={<Home/>}/>
-      {authToken && <Route path='/user/onboarding' element={<UserOnboarding/>}/>}
-      {authToken && <Route path='/rest/onboarding' element={<RestOnboarding/>}/>}
-      {authToken && <Route path='/user/dashboard' element={<UserDashboard/>}/>}
-      {authToken && <Route path='/rest/dashboard' element={<RestDashboard/>}/>}
+        <Route path="/" element={<Home />} />
+        {authToken && (
+          <Route path="/user/onboarding" element={<UserOnboarding />} />
+        )}
+        {authToken && (
+          <Route path="/rest/onboarding" element={<RestOnboarding />} />
+        )}
+        {authToken && (
+          <Route path="/user/dashboard" element={<UserDashboard />} />
+        )}
+        {authToken && (
+          <Route path="/rest/dashboard" element={<RestDashboard />} />
+        )}
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
