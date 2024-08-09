f271773d0a9899614d39f67be69b3de676b08fcc
ericmlantz <ericmlantz@gmail.com>
2024-08-09 11:41:50 -0400

    Updated to working but without url for backend in .env file

--- src/globals.js
+++ src/globals.js
@@ -1,2 +1,2 @@
 export const BACKEND =
-  'https://us-east-2.admin.amplifyapp.com/admin/login?appId=d3ajlh63tdn2ux&backendEnvironmentName=dev'
+  process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000'
